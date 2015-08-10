ae50aa2493d1861bf6b30274f8408573dc78c679
myaff <grabka74@gmail.com>
2015-08-10 15:07:12 +0500

    футер

--- footer.php
+++ footer.php
@@ -1,6 +1,49 @@
 </div> <!-- /.workarea -->
 	<div class="footer">
-	
+		<div class="footer-content">
+			<div class="row container">
+				<div class="footer-menus col l9 m8 hide-on-small-only">
+					<ul class="footer-menu col l4 m6">
+						<li class="menu-item"><a href="#" class="menu-link">О Ямале</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Новости</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Анонсы</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Меры поддержки</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Инфраструктура поддержки</a></li>
+					</ul>
+					<ul class="footer-menu col l4 m6">
+						<li class="menu-item"><a href="#" class="menu-link">О Проекте</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Наша группа</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Истории успеха</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Партнеры</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Контакты</a></li>
+					</ul>
+					<ul class="footer-menu col l4 m6">
+						<li class="menu-item"><a href="#" class="menu-link">Полезная информация</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Отправить заявку</a></li>
+						<li class="menu-item"><a href="#" class="menu-link">Предложить проект</a></li>
+					</ul>
+				</div>
+				<div class="footer-contacts col l3 m4 s12">
+					<p class="phone"><a class="nostyle" href="callto:+73492252825">+7&nbsp;(349)&nbsp;225&#8209;28&#8209;25</a></p>
+					<p class="email"><a class="nostyle" href="mailto:info@mb89.ru">info@mb89.ru</a></p>
+					<p class="address">г. Салехард, ул. Комсомольская, 13</p>
+					<p class="soc">
+						<a class="btn-icon small vk" href="#" target="_blank"><svg class="icon"><use xlink:href="#vk"</svg></a>
+						<a class="btn-icon small fb" href="#" target="_blank"><svg class="icon"><use xlink:href="#fb"</svg></a>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="footer-bottom hide-on-small-only">
+			<div class="row container">
+				<div class="copy col l6 m6">
+					© 2015. Финансовая группа поддержки СМСП ЯНАО 
+				</div>
+				<div class="dev col l6 m6 right-align">
+					Дизайн и разработка сайта - <a href="http://legacystudio.ru/" class="nostyle" target="_blank">Legacy</a>
+				</div>
+			</div>
+		</div>
 	</div>
 </div> <!-- /.page -->
 </div> <!-- /.layout -->
