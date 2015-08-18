4947daebeb146d35ef02e29c15385a58b91d6910
myaff <grabka74@gmail.com>
2015-08-18 20:35:46 +0500

    микрозайм

---
+++ microzaimy__single.php
@@ -0,0 +1,122 @@
+<?include('header.php');?>
+<div class="breadcrumbs">
+	<ul class="nostyle">
+		<li class="breadcrumbs-item"><a href="#" class="breadcrumbs-link">Меры поддержки</a></li>
+		<li class="breadcrumbs-item"><a href="#" class="breadcrumbs-link">Микрозаймы</a></li>
+	</ul>
+</div>
+<div class="sup-measures-single micro-z">
+	<div class="page-title">Мирозайм “Северный завод”</div>
+	<div class="item-content">
+		<div class="table-tabs hide-on-med-and-up">
+			<div class="select-box">
+				<select class="hide-on-med-and-up type-2" id="tabber" data-tab-content="micro-content">
+					<option value="v1" selected>Краткосрочный займ</option>
+					<option value="v2">Долгосрочный займ</option>
+				</select>
+				<label class="triangle"></label>
+			</div>
+			<div class="tab-content" id="micro-content">
+				<div class="content-item" data-tab="v1">
+					<div class="content-block">
+						<div class="item-title">Срок займа</div>
+						<div class="item-value">до 12 месяцев</div>
+					</div>
+					<div class="content-block">
+						<div class="item-title">Сумма займа</div>
+						<div class="item-value">от 100&nbsp;000 до 1&nbsp;000&nbsp;000 <span class="rouble">й</span></div>
+					</div>
+					<div class="content-block">
+						<div class="item-title">Процентная ставка</div>
+						<div class="item-value">6.0%</div>
+					</div>
+					<div class="content-block">
+						<div class="item-title">Отсрочка платежа по основной сумме займа</div>
+						<div class="item-value">до 9 месяцев</div>
+					</div>
+				</div>
+				<div class="content-item" data-tab="v2">
+					<div class="content-block">
+						<div class="item-title">Срок займа</div>
+						<div class="item-value">до 36 месяцев</div>
+					</div>
+					<div class="content-block">
+						<div class="item-title">Сумма займа</div>
+						<div class="item-value">от 100&nbsp;000 до 1&nbsp;000&nbsp;000 <span class="rouble">й</span></div>
+					</div>
+					<div class="content-block">
+						<div class="item-title">Процентная ставка</div>
+						<div class="item-value">9.0%</div>
+					</div>
+					<div class="content-block">
+						<div class="item-title">Отсрочка платежа по основной сумме займа</div>
+						<div class="item-value">до 12 месяцев</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<table class="zaim hide-on-small-only">
+			<tr>
+				<th></th>
+				<th>Краткосрочный микрозайм</th>
+				<th>Долгосрочный микрозайм</th>
+			</tr>
+			<tr>
+				<th>Срок займа</th>
+				<td>до 12 месяцев</td>
+				<td>до 36 месяцев</td>
+			</tr>
+			<tr>
+				<th>Сумма займа</th>
+				<td colspan="2">от 100&nbsp;000 до 1&nbsp;000&nbsp;000 <span class="rouble">й</span></td>
+			</tr>
+			<tr>
+				<th>Процентная ставка</th>
+				<td>6.0%</td>
+				<td>9.0%</td>
+			</tr>
+			<tr>
+				<th>Отсрочка платежа по основной сумме займа</th>
+				<td>до 9 месяцев</td>
+				<td>до 12 месяцев</td>
+			</tr>
+		</table>
+		<a href="#" class="btn primary center order-btn">Отправить заявку</a>
+		<div class="content-block">
+			<div class="block-title">Дополнительные документы</div>
+			<div class="row">
+				<div class="col l6 m12 s12">
+					<a class="file" href="#.pdf">Полное описание микрозайма “Северный завод”</a>
+				</div>
+				<div class="col l6 m12 s12">
+					<a class="file" href="#.pdf">Перечень документов для получения займа</a>
+				</div>
+			</div>
+		</div>
+		<div class="content-block">
+			<div class="block-title">Формы документов для получения микрозайма</div>
+			<div class="row">
+				<div class="col l6 m12 s12">
+					<a class="file" href="#.doc">Заявление анкета на получение финансовой услуги</a>
+				</div>
+				<div class="col l6 m12 s12">
+					<a class="file" href="#.doc">Отчет о прибытиях и убытках к заявлению</a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col l6 m12 s12">
+					<a class="file" href="#.doc">Баланс к заявлению</a>
+				</div>
+				<div class="col l6 m12 s12">
+					<a class="file" href="#.doc">Анкета поручителя</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="contact-info hide-on-small-only">
+		<div class="block-title">Нужна помощь?</div>
+		<p class="x-large">+7 (349) 225-28-25</p>
+		<p>Просто позвоните по этому номеру или <a href="#">оставьте заявку</a> на сайте</p>
+	</div>
+</div>
+<?include('footer.php');?>
