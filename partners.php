06cabbd8cc95f5be728d5ca8c48b92c2f1523c2f
myaff <grabka74@gmail.com>
2015-08-19 16:38:30 +0500

    партнеры
    каждый блок в них в принципе одинаков

---
+++ partners.php
@@ -0,0 +1,233 @@
+<?include('header.php');?>
+<div class="partners-page">
+	<div class="page-title">Партнеры</div>
+	<div class="block-title">Органы государственной власти</div>
+	<div class="row">
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Министерство экономического развития Российской Федерации</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-fns.png" />
+				<div class="item-title">Федеральная налоговая служба</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-minpromtorg.png" />
+				<div class="item-title">Министерство промышленности и торговли Российской Федерации</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-cb.png" />
+				<div class="item-title">Центральный банк Российской Федерации</div>
+			</a>
+		</div>
+	</div>
+	<div class="block-title">Контролирующие структуры</div>
+	<div class="row">
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Правительство Ямало-Ненецкого автономного округа</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент экономики ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент АПК ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент по науке и инновациям ЯНАО</div>
+			</a>
+		</div>
+	</div>
+	<div class="block-title">Государственные услуги</div>
+	<div class="row">
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Портал государственных закупок РФ</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент экономики ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент АПК ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент по науке и инновациям ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Портал государственных закупок РФ</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент экономики ЯНАО</div>
+			</a>
+		</div>
+	</div>
+	<div class="block-title">Бизнес-омбутсмен</div>
+	<div class="row">
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Правительство Ямало-Ненецкого автономного округа</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент экономики ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент АПК ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент по науке и инновациям ЯНАО</div>
+			</a>
+		</div>
+	</div>
+	<div class="block-title">Финансовые организации</div>
+	<div class="row">
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Правительство Ямало-Ненецкого автономного округа</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент экономики ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент АПК ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент по науке и инновациям ЯНАО</div>
+			</a>
+		</div>
+	</div>
+	<div class="block-title">Бизнес-сообщества</div>
+	<div class="row">
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Правительство Ямало-Ненецкого автономного округа</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент экономики ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент АПК ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент по науке и инновациям ЯНАО</div>
+			</a>
+		</div>
+	</div>
+	<div class="block-title">Общественные организации</div>
+	<div class="row">
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Правительство Ямало-Ненецкого автономного округа</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент экономики ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент АПК ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент по науке и инновациям ЯНАО</div>
+			</a>
+		</div>
+	</div>
+	<div class="block-title">Информационные партнеры</div>
+	<div class="row">
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Правительство Ямало-Ненецкого автономного округа</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент экономики ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент АПК ЯНАО</div>
+			</a>
+		</div>
+		<div class="item col l3 m4">
+			<a href="#">
+				<img class="item-logo" src="images/content/partner-logo-universal.png" />
+				<div class="item-title">Департамент по науке и инновациям ЯНАО</div>
+			</a>
+		</div>
+	</div>
+</div>
+<?include('footer.php');?>
