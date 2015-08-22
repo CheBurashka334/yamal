36def6890cf606fa62f2f55cff4bc7862cc26426
myaff <grabka74@gmail.com>
2015-08-22 13:52:59 +0500

    404

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?include('header.php');?>
+<div class="page-404 row">
+	<div class="col l6 m6 s12 item-img">
+		<img src="images/404.png"/>
+	</div>
+	<div class="col l6 m6 s12">
+		<div class="item-title large">Страница не найдена</div>
+		<p class="item-text">Вы можете вернуться <a href="#">назад</a> или <br/>перейти на <a href="#">главную</a></p>
+	</div>
+</div>
+<?include('footer.php');?>
